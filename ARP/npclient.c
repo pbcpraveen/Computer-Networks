5787d7ccd9b91c6724f3b22986a4152e0e6aa1af
pbcpraveen <pbcpraveen@gmail.com>
2019-09-15 21:20:14 +0530

    Modified ARP

--- ARP/npclient.c
+++ ARP/npclient.c
@@ -1,73 +1,78 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-//#include<strings.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(char **argc,int args)
 {
-  printf("start");
   char *mac, *ip;
   mac = (char*) malloc(100);
   ip = (char*) malloc(100);
   for(int i = 0; i<100;i++) ip[i] = '\0';
-  printf("before");
-  FILE* fp = popen("ifconfig -a | grep -Po 'HWaddr \\K.*$'", "r");
+  FILE* fp = popen("ifconfig | grep \"ether*\" | tr -d ' ' | tr -d '\\t' | cut -c 6-22|head -n 1", "r");
   if (fgets(mac, 99, fp) != NULL) {
-    printf("%s", mac);
+    printf("The clients mac is : %s\n", mac);
   }
-  printf("after");
+  mac[17] = '\0';
+
   
   fp =  popen("ip route get 8.8.4.4 | head -1 | awk '{print $7}'","r");
    if (fgets(ip, 99, fp) != NULL) {
-     printf("step3");
-     printf("%s",ip);
-   
+     printf("The client IP is : %s\n",ip);
+     ip[strlen(ip)-1] = '\0';
    }
-
+  
   fclose(fp);
-  printf("step3");
+ 
   //create socket
   int cfd = socket(AF_INET,SOCK_STREAM,0);
-  struct sockaddr_in *serveraddr,*clientaddr;
-  printf("step2");
-  //creating memory to the server and client address structures
-  serveraddr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
-  clientaddr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
-  printf("nitin");
+  struct sockaddr_in serveraddr,clientaddr;
+
+  
   // clear address memory
-  bzero(serveraddr,sizeof(struct sockaddr_in));
-  bzero(clientaddr,sizeof(struct sockaddr_in));
+  bzero(&serveraddr,sizeof(struct sockaddr_in));
+  bzero(&clientaddr,sizeof(struct sockaddr_in));
   
   // initialise address structure for server
-  bzero(serveraddr,sizeof(*serveraddr));
-  (*serveraddr).sin_family=AF_INET;
-  (*serveraddr).sin_addr.s_addr=inet_addr("10.6.9.11");
-  (*serveraddr).sin_port=htons(7889);
-  connect(cfd,(struct sockaddr*)serveraddr,sizeof((*serveraddr)));
+  bzero(&serveraddr,sizeof(serveraddr));
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+  serveraddr.sin_port=htons(7891);
+  connect(cfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
  
   char *buff;
   char* buff1 = "null";
-   char* buff2 = "null1";
+  char* buff2 = "null1";
   buff = (char*)malloc(100);
-  //write(cfd,buff2,100);
-  while(read(cfd,buff,100)<=0) ;
-  printf("%s,%d",buff,strlen(buff));/*
+
+  
+  //read the request from the server.
+  int n1 = 0;
+  while(n1 == 0)
+    n1 = read(cfd,buff,100);
+  buff[n1] = '\0';
+  printf("Received IP request is %s,%p\n",buff,ip);
+
+  // compare the client ip with the host ip
   if(strcmp(buff,ip)==0)
     {
-      write(cfd,mac,(sizeof(mac)));
+      printf("Server is ip matched....\n");
+      printf("Sending the mac address to server to initiate the communication\n");
+      write(cfd,mac,strlen(mac));
       read(cfd,buff,100);
-      printf("Received message: %s",buff);
+      printf("Received message: %s\n",buff);
     }
   
   else {
+    printf("ip mismatch\n");
       write(cfd,buff1,(sizeof(buff1)));
     }
-    close(cfd); */
+    close(cfd); 
   return 0;
 }
--- ARP/npserver.c
+++ ARP/npserver.c
@@ -7,27 +7,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(char **argc,int args)
 {
   //create socket
   int sfd = socket(AF_INET,SOCK_STREAM,0);
-  struct sockaddr_in *serveraddr,*clientaddr;
+  struct sockaddr_in serveraddr,clientaddr;
   
-  //creating memory to the server and client address structures
-  serveraddr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
-  clientaddr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
-  
+
   // clear address memory
-  bzero(serveraddr,sizeof(struct sockaddr_in));
-  bzero(clientaddr,sizeof(struct sockaddr_in));
+  bzero(&serveraddr,sizeof(struct sockaddr_in));
+  bzero(&clientaddr,sizeof(struct sockaddr_in));
   
   // initialise address structure for server
-  (*serveraddr).sin_family=AF_INET;
-  (*serveraddr).sin_addr.s_addr=INADDR_ANY;
-  (*serveraddr).sin_port=htons(7890);
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_addr.s_addr=INADDR_ANY;
+  serveraddr.sin_port=htons(7891);
 
   //bind the socket structure to its address
-  if (bind(sfd,serveraddr,sizeof(*serveraddr))<0)
+  if (bind(sfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr))<0)
     {
       perror("bind error");
       exit(0);
@@ -36,49 +34,55 @@ int main(char **argc,int args)
   //listen to incoming client connection
   listen(sfd,10);
 
-  char ch = 'y';
+  int ch = 1;
   int i = 0;
   int fd[10];
   char iplist[10][100];
   struct sockaddr_in clist[10];
-  while(ch == 'y')
+  while(ch == 1)
     {
-      int f1 = accept(sfd,*clientaddr,sizeof(struct sockaddr_in));
+      int len = sizeof(struct sockaddr_in);
+      int f1 = accept(sfd,(struct sockaddr *)&clientaddr,&len);
       fd[i] = f1;
       printf("%d",f1);
-      clist[i] = *clientaddr;
+      clist[i] = clientaddr;
       printf("\nnew connection received: ");
-      char *ip = inet_ntoa(clist[i].sin_addr);
-      strcpy(iplist[i],ip);
+      struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&clientaddr;
+      struct in_addr ipAddr = pV4Addr->sin_addr;
+      char str1[INET_ADDRSTRLEN];
+      inet_ntop( AF_INET, &ipAddr, str1, INET_ADDRSTRLEN );
+      strcpy(iplist[i],str1);
       i++;
-      printf(" %s\n",ip);
-      printf("\nenter y to connect to another client: ");
-      scanf("%c",&ch);
-      bzero(clientaddr,sizeof(struct sockaddr_in));
+      printf(" %s\n",str1);
+      sleep(2);
+      printf("\nenter 1 to connect to another client: ");
+      scanf("%d",&ch);
+      //bzero(&clientaddr,sizeof(struct sockaddr_in));
     }
 
   char* ipreq;
   char* macreply;
   macreply = (char*) malloc(100);
   ipreq = (char*) malloc(100);
-  printf("enter the ip to be requested: ");
+  printf("Enter the ip to be requested: ");
   scanf("%s",ipreq);
-  printf("%s\n",ipreq);
+  printf("Requested IP is : %s\n",ipreq);
   int total_connections =i;
   i = 0;
   while(i<total_connections){
     write(fd[i],ipreq,100);
     printf("request sent to:\t%s\n",iplist[i]);
-    read(fd[i],macreply,99);
-
+    sleep(5);
+    int nr = read(fd[i],macreply,99);
+    macreply[nr] = '\0';
     if(strcmp(macreply,"null") != 0){
       printf("\nrecieved mac of %s:\t%s\n",iplist[i],macreply);
-      char* buff = "10110110";
+      char buff[] = "10110110";
       write(fd[i],buff,sizeof(buff));
       i++;
     }
     else{
-      char* buff = "ip mismatch";
+      char buff[] = "ip mismatch";
       write(fd[i],buff,sizeof(buff));
     }
   }
